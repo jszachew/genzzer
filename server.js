fbb7801e58959cd6ce381371567ac197782d4d3a
Jakub Szachewicz <jszachewicz@vewd.com>
2022-04-07 17:35:20 +0200

    server done

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express()
 const bodyParser = require('body-parser');
 let port = process.env.PORT || 3000
 
+const cors = require('cors');
+const path = require('path');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.raw());
 app.use(express.json())
@@ -10,6 +13,11 @@ app.use(express.json())
 const api = require('./routes/api.routes')
 
 app.use('/api', api)
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  });
 
 app.listen(port, () =>{
     console.log(`running server on port ${port}`)
